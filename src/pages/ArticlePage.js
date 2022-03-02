035333f2179271ef532804c58009dee14579d3e4
MikeB1234 <mbacha2345@gmail.com>
2022-03-02 11:30:25 -0500

    add React hooks, add fetch to article pages

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
 import articleContent from './Article-Content'
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from './NotFoundPage';
 
 export const ArticlePage = () => {
+
     const { name } = useParams();
     const article = articleContent.find(article => article.name === name);
+
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            setArticleInfo(body);
+        }
+        fetchData();
+    }, [name]);
 
     if (!article) {
         return <NotFoundPage />
@@ -17,6 +29,7 @@ export const ArticlePage = () => {
     return (
         <>
             <h1>{article.title}</h1>
+            <p>This post has been upvoted {articleInfo.upvotes} times.</p>
             {article.content.map((paragraph, key) => {
                 return <p key={key}>{paragraph}</p>
             })}
